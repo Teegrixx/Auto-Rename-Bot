2d7d00f8b7d0787c450ad402d59d2a6f465f5d2f
teegrixx <89885361+Teegrixx@users.noreply.github.com>
2024-03-31 20:56:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,33 +20,31 @@ class Bot(Client):
         )
 
     async def start(self):
-        await super().start()
-        me = await self.get_me()
-        self.mention = me.mention
-        self.username = me.username  
-        self.uptime = Config.BOT_UPTIME     
-        if Config.WEBHOOK:
-            app = web.AppRunner(await web_server())
-            await app.setup()       
-            await web.TCPSite(app, "0.0.0.0", 8080).start()     
-        print(f"{me.first_name} Is Started.....✨️")
-        for id in Config.ADMIN:
-            try: await self.send_message(Config.LOG_CHANNEL, f"**{me.first_name}  Is Started.....✨️**")                                
-            except: pass
-        if Config.LOG_CHANNEL:
-            try:
-                curr = datetime.now(timezone("Asia/Kolkata"))
-                date = curr.strftime('%d %B, %Y')
-                time = curr.strftime('%I:%M:%S %p')
-                await self.send_message(Config.LOG_CHANNEL, f"**{me.mention} Is Restarted !!**\n\n📅 Date : `{date}`\n⏰ Time : `{time}`\n🌐 Timezone : `Asia/Kolkata`\n\n🉐 Version : `v{__version__} (Layer {layer})`</b>")                                
-            except:
-                print("Please Make This Is Admin In Your Log Channel")
+        try:
+            await super().start()
+            me = await self.get_me()
+            self.mention = me.mention
+            self.username = me.username  
+            self.uptime = Config.BOT_UPTIME     
+            if Config.WEBHOOK:
+                app = web.AppRunner(await web_server())
+                await app.setup()       
+                await web.TCPSite(app, "0.0.0.0", 8080).start()     
+            print(f"{me.first_name} Is Started.....✨️")
+            for id in Config.ADMIN:
+                try: await self.send_message(Config.LOG_CHANNEL, f"**{me.first_name}  Is Started.....✨️**")                                
+                except Exception as e:
+                    print(f"Failed to send start message to admin: {e}")
+            if Config.LOG_CHANNEL:
+                try:
+                    curr = datetime.now(timezone("Asia/Kolkata"))
+                    date = curr.strftime('%d %B, %Y')
+                    time = curr.strftime('%I:%M:%S %p')
+                    await self.send_message(Config.LOG_CHANNEL, f"**{me.mention} Is Restarted !!**\n\n📅 Date : `{date}`\n⏰ Time : `{time}`\n🌐 Timezone : `Asia/Kolkata`\n\n🉐 Version : `v{__version__} (Layer {layer})`</b>")                                
+                except Exception as e:
+                    print(f"Failed to send restart message to log channel: {e}")
+        except Exception as e:
+            print(f"An error occurred while starting the bot: {e}")
 
-Bot().run()
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Developer @JishuDeveloper
+if __name__ == "__main__":
+    Bot().run()
