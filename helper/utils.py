4661c03cfc5bb364d0be1165968dc9f3cb47af98
teegrixx <89885361+Teegrixx@users.noreply.github.com>
2024-03-31 21:01:13 +0300

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -1,51 +1,42 @@
-import math
-import time
+import math, time
 from datetime import datetime
 from pytz import timezone
+from config import Config, Txt 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from config import Config, Txt
 
-async def progress_for_pyrogram(current, total, update_type, message, start_time):
-    try:
-        now = time.time()
-        elapsed_time = now - start_time
-        if round(elapsed_time % 5.00) == 0 or current == total:
-            percentage = current * 100 / total
-            speed = current / elapsed_time
-            estimated_total_time = (total - current) / speed
+async def progress_for_pyrogram(current, total, ud_type, message, start):
+    now = time.time()
+    diff = now - start
+    if round(diff % 5.00) == 0 or current == total:        
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
 
-            progress_bar = generate_progress_bar(percentage)
-            formatted_elapsed_time = format_time(elapsed_time * 1000)
-            formatted_total_time = format_time(estimated_total_time * 1000)
+        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-            progress_message = (
-                f"{update_type}\n\n"
-                f"{progress_bar}{Txt.PROGRESS_BAR.format(percentage, humanbytes(current), humanbytes(total), humanbytes(speed), formatted_total_time)}"
-            )
-
+        progress = "{0}{1}".format(
+            ''.join(["⬢" for i in range(math.floor(percentage / 5))]),
+            ''.join(["⬡" for i in range(20 - math.floor(percentage / 5))])
+        )            
+        tmp = progress + Txt.PROGRESS_BAR.format( 
+            round(percentage, 2),
+            humanbytes(current),
+            humanbytes(total),
+            humanbytes(speed),            
+            estimated_total_time if estimated_total_time != '' else "0 s"
+        )
+        try:
             await message.edit(
-                text=progress_message,
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✖️ Cancel ✖️", callback_data="close")]])
+                text=f"{ud_type}\n\n{tmp}",               
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✖️ Cancel ✖️", callback_data="close")]])                                               
             )
-    except Exception as e:
-        print(f"Error in progress_for_pyrogram: {e}")
-
-def generate_progress_bar(percentage):
-    filled_blocks = ''.join(["⬢" for _ in range(math.floor(percentage / 5))])
-    empty_blocks = ''.join(["⬡" for _ in range(20 - math.floor(percentage / 5))])
-    return filled_blocks + empty_blocks
-
-def format_time(milliseconds):
-    seconds, milliseconds = divmod(int(milliseconds), 1000)
-    minutes, seconds = divmod(seconds, 60)
-    hours, minutes = divmod(minutes, 60)
-    days, hours = divmod(hours, 24)
-    formatted_time = ((str(days) + "d, ") if days else "") + \
-                     ((str(hours) + "h, ") if hours else "") + \
-                     ((str(minutes) + "m, ") if minutes else "") + \
-                     ((str(seconds) + "s, ") if seconds else "") + \
-                     ((str(milliseconds) + "ms, ") if milliseconds else "")
-    return formatted_time[:-2] 
+        except:
+            pass
+            
+            
 
 def humanbytes(size):    
     if not size:
@@ -58,25 +49,39 @@ def humanbytes(size):
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'b'
 
-async def send_log(bot, user):
-    try:
-        if Config.LOG_CHANNEL is not None:
-            curr = datetime.now(timezone("Asia/Kolkata"))
-            date = curr.strftime('%d %B, %Y')
-            time = curr.strftime('%I:%M:%S %p')
-            await bot.send_message(
-                Config.LOG_CHANNEL,
-                f"<b><u>New User Started The Bot</u></b> \n\n"
-                f"<b>User ID</b> : `{user.id}` \n"
-                f"<b>First Name</b> : {user.first_name} \n"
-                f"<b>Last Name</b> : {user.last_name} \n"
-                f"<b>User Name</b> : @{user.username} \n"
-                f"<b>User Mention</b> : {user.mention} \n"
-                f"<b>User Link</b> : <a href='tg://openmessage?user_id={user.id}'>Click Here</a>\n\n"
-                f"Date: {date}\nTime: {time}\n\nBy: {bot.mention}"
-            )
-    except Exception as e:
-        print(f"Error in send_log: {e}")
+
+def TimeFormatter(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s, ") if seconds else "") + \
+        ((str(milliseconds) + "ms, ") if milliseconds else "")
+    return tmp[:-2] 
+
+def convert(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60      
+    return "%d:%02d:%02d" % (hour, minutes, seconds)
+
+async def send_log(b, u):
+    if Config.LOG_CHANNEL is not None:
+        curr = datetime.now(timezone("Asia/Kolkata"))
+        date = curr.strftime('%d %B, %Y')
+        time = curr.strftime('%I:%M:%S %p')
+        await b.send_message(
+            Config.LOG_CHANNEL,
+            f"<b><u>New User Started The Bot</u></b> \n\n<b>User ID</b> : `{u.id}` \n<b>First Name</b> : {u.first_name} \n<b>Last Name</b> : {u.last_name} \n<b>User Name</b> : @{u.username} \n<b>User Mention</b> : {u.mention} \n<b>User Link</b> : <a href='tg://openmessage?user_id={u.id}'>Click Here</a>\n\nDate: {date}\nTime: {time}\n\nBy: {b.mention}"
+        )
+        
+
+
 
 # Jishu Developer 
 # Don't Remove Credit 🥺
